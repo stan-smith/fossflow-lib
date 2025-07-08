1d4ab52b21ec6b435f36fa38d801eb227238135b
Abhinav Kumar <abhinavkumar130503@gmail.com>
2025-07-09 00:19:05 +0530

    test: add and update Jest snapshot files for DebugUtils component tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
-  modulePaths: ['node_modules', '<rootDir>']
+  testEnvironment: "jsdom",
+  modulePaths: ['node_modules', '<rootDir>'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 };
